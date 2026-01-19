6f5d62b37fef221e98f9d900ee5179d74266ecb8
Aymogul <aymogul22@gmail.com>
2026-01-20 00:18:21 +0100

    route table

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -20,7 +20,28 @@ map_public_ip_on_launch = true
 }
 
 # Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "My_IGW"
+  }
+}
 
 # Route Table
+resource "aws_route_table" "rt" {   
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0" # public access
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "My_Route_Table"
+  }
+}
 
 # Route Table Association
+resource "aws_route_table_association" "rta" {
+  count          = length(var.subnet_cidrs)
+  subnet_id      = aws_subnet.subnets[count.index].id
+  route_table_id = aws_route_table.rt.id
+}

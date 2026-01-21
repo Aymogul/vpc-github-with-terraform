efa4a2eb0ae9dc300fc86d092ebc6c1ce6a30cc0
Aymogul <aymogul22@gmail.com>
2026-01-21 13:18:43 +0100

    sg module done

--- Terraform-VPC/modules/vpc/outputs.tf
+++ Terraform-VPC/modules/vpc/outputs.tf
@@ -1,6 +1,6 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.my_vpc.id
 }
 output "subnet_ids" {
   description = "List of Subnet IDs"
